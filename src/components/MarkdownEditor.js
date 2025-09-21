abbe55a7cd4bf937cf60ff0b69ca3a378a649ba0
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-21 11:48:53 +0530

    initial commit - markdown

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 
 function MarkdownEditor() {
-  const [markdown, setMarkdown] = useState("# Heading");
+  const [markdown, setMarkdown] = useState("# Heading\n");
   const [preview, setPreview] = useState("");
   useEffect(() => {
     setPreview(markdown);
