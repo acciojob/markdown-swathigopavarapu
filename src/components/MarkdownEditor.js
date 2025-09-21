bf56ed9250df482415248ff05076b8d77f8d5538
Swathi Gopavarapu <swathigopavarapu1@gmail.com>
2025-09-21 12:51:10 +0530

    initial commit - markdown

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 function MarkdownEditor() {
-  const [markdown, setMarkdown] = useState("# Heading"); 
+  const [markdown, setMarkdown] = useState("Heading"); 
 
   return (
     <div className="editor-container">
